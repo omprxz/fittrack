40bf27b5e0ead1db950519f54f7e2e955fb6316d
Om <omprxz@gmail.com>
2024-08-02 21:25:18 +0530

    Migrate from render to mongodb with some config changes

--- backend/db/config.js
+++ backend/db/config.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 async function connectToMongoDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected successfully.');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
   }
